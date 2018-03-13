551b8a5b581ffd153a7fe3716f9e2cc2193d134d
Andrea Ortu <andrea.ortu@greencommservices.it>
2018-03-13 12:55:59 +0100

    Initial Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laraveladasd
                 </div>
 
                 <div class="links">
